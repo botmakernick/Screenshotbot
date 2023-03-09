beba846bde22e8d2a31e4774580235e610ca76b1
botmakernick <127219495+botmakernick@users.noreply.github.com>
2023-03-09 18:32:16 +0530

    Update database.py

--- bot/database/database.py
+++ bot/database/database.py
@@ -18,7 +18,7 @@ class Singleton(type):
 class Database(metaclass=Singleton):
     def __init__(self):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(Config.DATABASE_URL)
-        self.db = self._client[Config.SESSION_NAME]
+        self.db = self._client[Config.SESSION_STRING]
         self.col = self.db.users
 
         self.cache = {}
